930d829762501e2fffdf7b01b2910a587448d202
geslain.dahan <geslain.dahan@keyrus.com>
2019-07-08 23:52:55 +0200

    :sparkles: Update Commentary page with CommentaryForm

--- src/components/commentaryPage/commentaryPage.js
+++ src/components/commentaryPage/commentaryPage.js
@@ -1,22 +1,46 @@
 import React, {useEffect, useState} from 'react';
 import Commentary from "../commentary/commentary";
 import commentaryService from "../../services/commentary"
+import CommentaryForm from "../commentaryForm/commentaryForm";
+import {Paper} from "@material-ui/core";
 
 function CommentaryPage() {
     const [commentaryList, setCommentaryList] = useState([])
 
     useEffect(() => {
         setCommentaryList(commentaryService.getCommentaryList())
-    }, [commentaryList]);
+    }, []);
 
-    const renderCommentaryList = (commentaryList) => {
-        return commentaryList.map(commentary =>
-            <Commentary {...commentary} key={commentary.id}/>
-        )
+    /**
+     * Add a submitted comment
+     *
+     * @param values
+     */
+    const handleCommentaryFormSubmit = (values) => {
+        commentaryService.addCommentary({
+            ...values,
+            date: new Date().toDateString()
+        }).then((a) => {
+            setCommentaryList(c => [...c, a])
+        }).catch()
     }
+
+    /**
+     * render the commentary list
+     * @param commentaryList
+     * @returns {*}
+     */
+    const renderCommentaryList = (commentaryList) =>
+        commentaryList.map(commentary =>{
+            return <Commentary {...commentary} key={commentary.id}/>
+        })
+
     return (
         <div>
             {renderCommentaryList(commentaryList)}
+            <Paper>
+                <CommentaryForm onSubmit={handleCommentaryFormSubmit}/>
+            </Paper>
         </div>
     );
 }
