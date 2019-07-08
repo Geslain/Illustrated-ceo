151b3c44511684c9a81f1a149795c3517d71437c
geslain.dahan <geslain.dahan@keyrus.com>
2019-07-08 23:49:51 +0200

    :sparkles: Adding Commentary form component and styles

---
+++ src/components/commentaryForm/styles.js
@@ -0,0 +1,13 @@
+export default () => ({
+    authorField: {
+        width: 200,
+    },
+    textField: {
+        margin: "10px 10px 30px 0",
+    },
+    paper: {
+        padding: 10,
+        display: "flex",
+        flexDirection: "column"
+    }
+})
